507bfab1c7eb70c5844c60dc98e76ccb8690036e
Morgan <morgannetherway1234@gmail.com>
2017-10-17 10:42:51 +0100

    Changes

--- hello.py
+++ hello.py
@@ -1,9 +1,4 @@
 print("Hello")
 print("Changes in another")
 print("World")
-<<<<<<< HEAD
 print("More change")
-print("Still more changes")
-=======
-print("More changes")
->>>>>>> 5ea479e8ee3fcd80555d3bacdb4f25c29f68e5a4
